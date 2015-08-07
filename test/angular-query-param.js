116f9e963bc56b86fdfe6f86846759f6d24b8885
Ins <ins@weblapbolt.hu>
2015-08-07 23:56:48 +0200

    refactor(queryParam): refactor test file

--- test/angular-query-param.js
+++ test/angular-query-param.js
@@ -15,46 +15,46 @@ describe('angularQueryParam', function () {
     expect(queryParam).toBeDefined();
   });
 
-  it('should set a simple parameter to the url', function () {
-    var key = 'testParam',
+  describe('with single item', function () {
+    var key,
+      value,
+      searchMock;
+
+    beforeEach(function () {
+      key = 'testParam';
       value = 'testValue';
-    spyOn($location, 'search');
+      searchMock = {};
+      searchMock[key] = value;
+    });
 
-    queryParam.set(key, value);
-    expect($location.search).toHaveBeenCalledWith(key, value);
+    it('should set a parameter to the url', function () {
+      spyOn($location, 'search');
+
+      queryParam.set(key, value);
+      expect($location.search).toHaveBeenCalledWith(key, value);
+    });
+
+    it('should get a parameter from the url', function () {
+      spyOn($location, 'search').and.returnValue(searchMock);
+
+      expect(queryParam.get(key)).toEqual(value);
+      expect(queryParam.get(value)).toBeUndefined();
+    });
+
+    it('should tell if a key is set', function () {
+      spyOn($location, 'search').and.returnValue(searchMock);
+
+      expect(queryParam.has(key)).toBe(true);
+      expect(queryParam.has(value)).toBe(false);
+    });
+
+    it('should tell if a key is set with a value', function () {
+      spyOn($location, 'search').and.returnValue(searchMock);
+
+      expect(queryParam.has(key, value)).toBe(true);
+      expect(queryParam.has(key, key)).toBe(false);
+    });
   });
 
-  it('should get a simple parameter from the url', function () {
-    var key = 'testParam',
-      value = 'testValue',
-      mockSearch = {};
-    mockSearch[key] = value;
-    spyOn($location, 'search').and.returnValue(mockSearch);
-
-    expect(queryParam.get(key)).toEqual(value);
-    expect(queryParam.get(value)).toBeUndefined();
-  });
-
-  it('should tell if a key is set', function () {
-    var key = 'testParam',
-      value = 'testValue',
-      mockSearch = {};
-    mockSearch[key] = value;
-    spyOn($location, 'search').and.returnValue(mockSearch);
-
-    expect(queryParam.has(key)).toBe(true);
-    expect(queryParam.has(value)).toBe(false);
-  });
-
-  it('should tell if a key is set with a value', function () {
-    var key = 'testParam',
-      value = 'testValue',
-      mockSearch = {};
-    mockSearch[key] = value;
-    spyOn($location, 'search').and.returnValue(mockSearch);
-
-    expect(queryParam.has(key, value)).toBe(true);
-    expect(queryParam.has(key, key)).toBe(false);
-  });
 
 });
